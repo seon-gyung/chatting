2aa60654457acb4bfd3b1ae1585fdad5a6f36c80
gyung <282203@naver.com>
2022-02-15 10:29:02 +0900

    2. 소켓 통신 키보드로 계속 입력받기

--- src/site/metacoding/chat/MyClientSocket.java
+++ src/site/metacoding/chat/MyClientSocket.java
@@ -3,26 +3,36 @@ package site.metacoding.chat;
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
-public class MyClientSocket01 {
+public class MyClientSocket {
 
     Socket socket;
     BufferedWriter writer;
+    Scanner sc;
 
-    public MyClientSocket01() {
+    public MyClientSocket() {
         try {
             socket = new Socket("localhost", 1077);
+
             writer = new BufferedWriter(
                 new OutputStreamWriter(socket.getOutputStream())
             );
-            writer.write("이선경\n"); // 윈도우 os는 \n을 써야 데이터가 보내짐
-            writer.flush(); // 버퍼 비우기
+
+            // 스캐너 달고
+            Scanner sc = new Scanner(System.in);
+            // 키보드 입력 받기 (반복)
+            while(true){
+                String inputData = sc.nextLine();
+                writer.write(inputData + "\n"); // 윈도우 os는 \n을 써야 데이터가 보내짐
+                writer.flush(); // 버퍼 비우기
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
     
     public static void main(String[] args) {
-        new MyClientSocket01();
+        new MyClientSocket();
     }
 }
--- src/site/metacoding/chat/MyServerSocket.java
+++ src/site/metacoding/chat/MyServerSocket.java
@@ -5,14 +5,14 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class MyServerSocket01 {
+public class MyServerSocket {
 
     ServerSocket serverSocket; // 리스너 : 연결되는지 주시하는 포트. (연결 = 세선)
     Socket socket; // 메시지 통신 : 통신하는 포트
 
     BufferedReader reader;
 
-    public MyServerSocket01() {
+    public MyServerSocket() {
         try {
             // 1. 서버 소켓 생성 (리스너)
             // well known 포트 : 0~1023 사용 금지. well known 포트가 아니어도 1521(오라클 포트)같은 건 충돌나서 사용 금지
@@ -22,10 +22,16 @@ public class MyServerSocket01 {
             reader = new BufferedReader(
                 new InputStreamReader(socket.getInputStream())
             );
-
-            String inputData =  reader.readLine(); // 버퍼 읽기
-            System.out.println("받은 메세지 : " + inputData);
-            System.out.println("클라이언트 연결됨");
+            // 버퍼 읽는거 반복
+            while(true){
+                String inputData =  reader.readLine(); // 버퍼 읽기
+                System.out.println("받은 메세지 : " + inputData);
+                if(inputData.equals("종료")){
+                    System.out.println("종료합니다");
+                    break;
+                }
+            }
+            // System.out.println("클라이언트 연결됨");
         } catch (Exception e) {
             System.out.println("통신 오류 발생 : " + e.getMessage());
             // e.printStackTrace(); 자세한 오류 내용
@@ -33,7 +39,7 @@ public class MyServerSocket01 {
     }
 
     public static void main(String[] args) {
-        new MyServerSocket01();
+        new MyServerSocket();
         System.out.println("메인 종료");
     }
 }
