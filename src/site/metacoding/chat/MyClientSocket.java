9074b3bc0811732d65b665ae8f6a93e2c1a5586e
gyung <282203@naver.com>
2022-02-15 11:59:20 +0900

    3. 소켓통신 전이중 통신

--- src/site/metacoding/chat/MyClientSocket.java
+++ src/site/metacoding/chat/MyClientSocket.java
@@ -1,6 +1,9 @@
 package site.metacoding.chat;
 
+
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.Scanner;
@@ -11,24 +14,43 @@ public class MyClientSocket {
     BufferedWriter writer;
     Scanner sc;
 
+    // 추가 (서버한테 메시지 받기)
+    BufferedReader reader;
+
     public MyClientSocket() {
         try {
             socket = new Socket("localhost", 1077);
-
+            // 메시지 쓰는 버퍼 달기
             writer = new BufferedWriter(
-                new OutputStreamWriter(socket.getOutputStream())
-            );
+                new OutputStreamWriter(socket.getOutputStream()));
+            // 메시지 읽는 버퍼 달기
+            reader = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
+            
+            // 버퍼 읽기 
+            new Thread(()->{
+                while(true){
+                    try {
+                        String inputData = reader.readLine();
+                        System.out.println("서버한테 받은 메시지 : " + inputData);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
 
             // 스캐너 달고
-            Scanner sc = new Scanner(System.in);
-            // 키보드 입력 받기 (반복)
-            while(true){
+            sc = new Scanner(System.in);        
+
+            // 키보드로 입력 받고 서버한테 보내기 반복 - 메인 스레드가 하는 일
+            while (true) {
                 String inputData = sc.nextLine();
-                writer.write(inputData + "\n"); // 윈도우 os는 \n을 써야 데이터가 보내짐
+                writer.write(inputData + "\n");
                 writer.flush(); // 버퍼 비우기
-            }
+            }          
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("통신 오류 발생 : " + e.getMessage());
+            // e.printStackTrace();
         }
     }
     
--- src/site/metacoding/chat/MyServerSocket.java
+++ src/site/metacoding/chat/MyServerSocket.java
@@ -1,16 +1,22 @@
 package site.metacoding.chat;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class MyServerSocket {
 
-    ServerSocket serverSocket; // 리스너 : 연결되는지 주시하는 포트. (연결 = 세선)
+    ServerSocket serverSocket; // 리스너 : 연결되는지 주시하는 포트. (연결 = 세션)
     Socket socket; // 메시지 통신 : 통신하는 포트
-
     BufferedReader reader;
+
+    //추가 (클라이언트한테 메시지 보내기)
+    BufferedWriter writer;
+    Scanner sc; // 키보드에서 입력 받은 걸 보낼거라서 스캐너 필요
 
     public MyServerSocket() {
         try {
@@ -20,16 +26,30 @@ public class MyServerSocket {
             System.out.println("서버 소켓 생성됨");
             socket = serverSocket.accept(); // while을 돌면서 1077번 포트로 들어오는 것을 대기함. (랜덤 포트)
             reader = new BufferedReader(
-                new InputStreamReader(socket.getInputStream())
-            );
-            // 버퍼 읽는거 반복
+                new InputStreamReader(socket.getInputStream()));
+
+            // 스캐너 달고
+            sc = new Scanner(System.in);
+            writer = new BufferedWriter(
+                new OutputStreamWriter(socket.getOutputStream()));
+
+            // 키보드로 입력 받아서 클라이언트한테 메시지 보내기 (반복)
+            new Thread(()->{
+                while(true){
+                    try {
+                        String inputData = sc.nextLine();
+                        writer.write(inputData+"\n");
+                        writer.flush(); // 버퍼 비우기
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+            // 버퍼 읽기 반복 - 메인 스레드가 하는 일
             while(true){
                 String inputData =  reader.readLine(); // 버퍼 읽기
-                System.out.println("받은 메세지 : " + inputData);
-                if(inputData.equals("종료")){
-                    System.out.println("종료합니다");
-                    break;
-                }
+                System.out.println("클라이언트한테 받은 메세지 : " + inputData);
             }
             // System.out.println("클라이언트 연결됨");
         } catch (Exception e) {
